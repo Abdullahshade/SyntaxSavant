b9e529a3caf1f6c6ab8635411024bee351c0ef5e
NoorTantawi <127735819+NoorTantawi@users.noreply.github.com>
2023-05-12 23:05:45 +0300

    Add files via upload
    uploaded

---
+++ Librarian.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Project
+{
+    public class Librarian
+    {
+
+        static List<Book> books = new List<Book>();
+
+        private string[] workSchedule;
+        private string emailAddress;
+
+        public Librarian(string[] workSchedule = null, string emailAddress = "")
+        {
+
+            this.workSchedule = workSchedule ?? new string[0];
+            this.emailAddress = emailAddress;
+        }
+
+        public static Book FindBookByTitle(string title)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Title == title)
+                {
+                    return book;
+                }
+
+            }
+            return null;
+        }
+
+        public void AddNewBook(string title, string author, int publicationYear, string genre, int isbn, int copies)
+        {
+
+
+
+
+            for (int j = 1; j <= copies; j++)
+            {
+
+                Book book = new Book(title, author, publicationYear, genre, isbn, 1);
+                books.Add(book);
+            }
+
+            Console.WriteLine("New book added: " + title);
+        }
+        public void Numberofbooks()
+        {
+            Console.WriteLine("The number of books in the library: " + books.Count);
+        }
+
+
+        public void RemoveBook(string title)
+        {
+
+
+
+            string searchTitle = title;
+            Book foundBook = books.Find(book => book.Title == searchTitle);
+
+            if (foundBook != null)
+            {
+                Console.WriteLine("Book found:");
+                // foundBook.ShowBookInfo();
+                books.Remove(foundBook);
+                Console.WriteLine("Book removed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Book not found.");
+            }
+
+
+        }
+    }
+}
---
+++ Patron.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Project
+{
+    class Patron
+    {
+        string[] ContactInformation;
+        List<Book> BorrowedItems;
+
+        public Patron()
+        {
+            ContactInformation = new string[0];
+            BorrowedItems = new List<Book>();
+        }
+
+        public bool RequestBorrowing()
+        {
+            Console.Write("Enter the title of the book you want to borrow: ");
+            string title = Console.ReadLine();
+
+
+            Book book = Librarian.FindBookByTitle(title);
+            if (book != null)
+            {
+
+                if (book.GetAvailabilityStatus() && book.GetCopies() > 0)
+                {
+                    int cout = book.GetCopies();
+                    BorrowedItems.Add(book);
+                    book.SetCopies(cout--);
+
+                    Console.WriteLine("Book borrowed successfully.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("The book is not available for borrowing.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Book not found in the library.");
+            }
+
+            return false;
+        }
+
+
+        public void RequestReservation()
+        {
+            Console.Write("Enter the title of the book you want to reserve: ");
+            string title = Console.ReadLine();
+
+            // Check if a book is available in the library by title
+            Book book = Librarian.FindBookByTitle(title);
+            if (book != null)
+            {
+                if (book.GetAvailabilityStatus() && book.GetCopies() > 0)             // Check availability of copies available for reservation
+                {
+                    BorrowedItems.Add(book);
+
+                    Console.WriteLine("Book reserved successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("The book is not available for reservation.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Book not found in the library.");
+            }
+        }
+
+    }
+}
